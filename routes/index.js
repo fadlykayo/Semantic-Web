000737c76e365d4a47ffc6efae0ff0b4232bb760
Fadly Kayo <f_kayo@yahoo.com>
2017-02-04 18:48:39 +0700

    Done Header and Footer

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,28 @@ router.get('/', function (req, res, next) {
   res.render('pages/index')
 })
 
-router.get('/logout', function (req, res, next) {
-  res.redirect('/')
-})
-
-router.get('/home', function (req, res, next) {
-  res.render('pages/menuadmin')
-})
-
-router.post('/login', function (req, res, next) {
-  res.redirect('home')
-})
-
-router.post('/logout', function (req, res, next) {
-  res.redirect('/')
-})
-
-router.get('/register', function (req, res, next) {
-  res.render('pages/register')
-})
-
-router.post('/register', function (req, res, next) {
-  res.redirect('/')
-})
+// router.get('/logout', function (req, res, next) {
+//   res.redirect('/')
+// })
+//
+// router.get('/home', function (req, res, next) {
+//   res.render('pages/menuadmin')
+// })
+//
+// router.post('/login', function (req, res, next) {
+//   res.redirect('home')
+// })
+//
+// router.post('/logout', function (req, res, next) {
+//   res.redirect('/')
+// })
+//
+// router.get('/register', function (req, res, next) {
+//   res.render('pages/register')
+// })
+//
+// router.post('/register', function (req, res, next) {
+//   res.redirect('/')
+// })
 
 module.exports = router
